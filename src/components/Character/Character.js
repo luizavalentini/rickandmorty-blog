ddccbcf41c99afa531b39f83845efecca80e6a9f
Luiza Valentini <luizadaravalentini@gmail.com>
2023-10-13 20:06:47 -0300

    last fix

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,11 +1,11 @@
 
 export function Character(character) {
-    return (
-      <div className="text-center p-5">
-        <h3>{character.name}</h3>
-        <img src={character.image} alt={character.name} className="img-fluid rounded-pill" />
-        <p>{`Origin: ${character.origin && character.origin.name}`}</p>
-      </div>
-    );
-  }
-  export default Character;
+  return (
+    <div className="text-center p-5">
+      <h3 className="pb-2">{character.name}</h3>
+      <img src={character.image} alt={character.name} className="img-fluid rounded-pill pt-3" />
+      <p className="pt-3">{`Origin: ${character.origin && character.origin.name}`}</p>
+    </div>
+  );
+}
+export default Character;
