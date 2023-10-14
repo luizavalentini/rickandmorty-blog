275edcdc39c3a9f71dedf55de4150ef0ce998497
Luiza Valentini <luizadaravalentini@gmail.com>
2023-10-13 22:16:47 -0300

    changing name

--- src/pages/SobreMim/index.js
+++ src/pages/SobreMim/index.js
@@ -1,11 +1,11 @@
 import CharacterList from "components/Character/CharacterList";
-function SobreMim () {
-    return(
-        <>  
-    <div className="bg-dark text-white">
-      <h1 className="text-center display-1 py-4">Personagens</h1>
-      <CharacterList />
-    </div>
+function SobreMim() {
+    return (
+        <>
+            <div className="bg-dark text-white">
+                <h1 className="text-center display-1 py-4">Characters</h1>
+                <CharacterList />
+            </div>
         </>
     );
 }
